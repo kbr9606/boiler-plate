a37d19a19bfd24e745727bed691e06f067ecafc4
kbr9606 <kbr9606@naver.com>
2022-07-18 22:41:30 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://boram:1234@cluster0.e0ig4l7.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log('err'))
 
 
-app.get('/', (req, res) => res.send('Hello World!!!!!'))
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({ success: true })
+  })
+})
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
